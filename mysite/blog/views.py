8ca0f98c62fccfd30425fa85fc831b1bad41b650
Evgenii <evgeniikatunkin@gmail.com>
2023-05-16 18:19:09 +0200

    Transfer app to postgres db. Add search functionality, page and postgres setings.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,20 +1,13 @@
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm
-from .models import Post, Comment
-
-
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+from .forms import EmailPostForm, CommentForm, SearchForm
+from .models import Post
 
 
 @require_POST
@@ -59,6 +52,19 @@ def post_list(request, tag_slug=None):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts, 'tag': tag})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query), ).filter(
+                similarity__gt=0.1).order_by('-similarity')
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
 
 def post_share(request, post_id):
