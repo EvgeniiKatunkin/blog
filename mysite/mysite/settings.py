4f5c3b0a0991469bb13bc1569b7ea04abe484cdf
Evgenii <evgeniikatunkin@gmail.com>
2023-05-16 15:01:24 +0200

    Add map of the site (blog/sitemaps.py).

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+SITE_ID = 1
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'taggit',
     'blog.apps.BlogConfig',
 ]
