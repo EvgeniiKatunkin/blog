1a3def2483c388f37fb6ba351c839bb9edbfbe19
Evgenii <evgeniikatunkin@gmail.com>
2023-05-16 18:20:17 +0200

    Adjust settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': '357a5S',
+        'PASSWORD': '',
     }
 }
 
